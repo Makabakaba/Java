8077cdad50fc1c2ccce83d14174ec607adcfa1d1
Makabakaba <xzs404@163.com>
2020-09-29 22:41:14 +0800

    Second assignment

---
+++ src/testone/ShowLogicErrors.java
@@ -0,0 +1,12 @@
+package testone;
+
+public class ShowLogicErrors {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.print("Celsius 35 is Fahrenheit degree ");
+		System.out.println((9.0 / 5) * 35 + 32);
+	}
+
+}
+// 9/5结果是1，9.0/5结果是1.8
