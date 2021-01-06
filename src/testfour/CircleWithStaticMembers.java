19b0e18ce5239b418f7585a7c66af2e6353b681d
HEW <HEW@LAPTOP-EHVGKN6P>
2021-01-06 23:44:14 +0800

    第6次作业：第九章课本例题

---
+++ src/testfour/CircleWithStaticMembers.java
@@ -0,0 +1,24 @@
+package testfour;
+
+public class CircleWithStaticMembers {
+	double radius;
+	static int number0f0bjects = 0;
+	
+	CircleWithStaticMembers(){
+		radius = 1;
+		number0f0bjects++;
+	}
+	
+	CircleWithStaticMembers(double  newRadius){
+		radius = newRadius;
+		number0f0bjects++;
+	}
+	
+	static int getNumber0f0bjects() {
+		return number0f0bjects;
+	}
+	
+	double getArea() {
+		return radius*radius*Math.PI;
+	}
+}
